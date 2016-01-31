2d2ae16bc9d6ab503f8daff6c237fb2e4704fe55
Andrew Kohnen <kohnen.andrew@live.ca>
2016-01-30 19:18:07 -0500

    Main.java
    Runs the Control classes constructor, which in turn runs the program

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	/*RUN PROGRAM FROM HERE
+	 * This programs implements the model make to solve the smokers concurrency problem 
+	 */
+	public static void main(String[] args) {
+		Control run = new Control();
+	}
+
+}
